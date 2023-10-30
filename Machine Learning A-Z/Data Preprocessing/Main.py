075dc8ddff7499297ad0f06d94657e444544f54f
Sami Munir <smunir2001@gmail.com>
2023-10-30 10:39:29 -0400

    importing the dataset

--- Machine Learning A-Z/Data Preprocessing/Main.py
+++ Machine Learning A-Z/Data Preprocessing/Main.py
@@ -9,3 +9,10 @@ print('\nData Preprocessing')
 import pandas as pd
 
 # Importing the dataset
+dataset = pd.read_csv('./data/Data.csv')
+print('\n./data/Data.csv')
+print('dataset -->\n', dataset)
+X = dataset.iloc[:, :-1].values # all columns & rows for features.
+y = dataset.iloc[:, -1].values # last column & all rows for independent variable vector
+print('\nX -->\n', X)
+print('\ny -->\n', y)
