de3c6f66c6e3796f0d0ff54ed54dbdfa505693c6
Sami Munir <smunir2001@gmail.com>
2023-10-30 12:01:43 -0400

    importing dataset

--- Machine Learning A-Z/Regression/Main.py
+++ Machine Learning A-Z/Regression/Main.py
@@ -14,6 +14,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
-
 # Importing the dataset
+dataset = pd.read_csv('./data/Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+print('\nX -->\n', X)
+print('\ny -->\n', y)
