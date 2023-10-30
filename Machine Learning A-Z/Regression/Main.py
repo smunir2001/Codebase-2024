0d0e12652ef2d16967d641f84c845968ff6521a4
Sami Munir <smunir2001@gmail.com>
2023-10-30 12:00:07 -0400

    importing libraries for regression

--- Machine Learning A-Z/Regression/Main.py
+++ Machine Learning A-Z/Regression/Main.py
@@ -8,3 +8,12 @@ print('\nRegression')
 # Simple Linear Regression
 print('\nSimple Linear Regression')
 print('------------------------')
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+
+# Importing the dataset
