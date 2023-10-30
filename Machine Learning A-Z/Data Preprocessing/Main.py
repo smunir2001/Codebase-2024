d8043f852bfae70d9e0ab6af20aaf7725a3419ad
Sami Munir <smunir2001@gmail.com>
2023-10-30 10:45:38 -0400

    taking care of missing data

--- Machine Learning A-Z/Data Preprocessing/Main.py
+++ Machine Learning A-Z/Data Preprocessing/Main.py
@@ -7,6 +7,8 @@ print('\nData Preprocessing')
 
 # Importing the libraries
 import pandas as pd
+# from sklearn.impute import SimpleImputer
+# import numpy as np
 
 # Importing the dataset
 dataset = pd.read_csv('./data/Data.csv')
@@ -16,3 +18,12 @@ X = dataset.iloc[:, :-1].values # all columns & rows for features.
 y = dataset.iloc[:, -1].values # last column & all rows for independent variable vector
 print('\nX -->\n', X)
 print('\ny -->\n', y)
+
+# Taking care of missing data
+from sklearn.impute import SimpleImputer
+import numpy as np
+print('\nX -->\n', X)
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(X[:, 1:3]) # capture all numerical valued columns (exclude any text valued columns)
+X[:, 1:3] = imputer.transform(X[:, 1:3]) # updating the numerical valued columns in matrix of features X
+print('\nupdated X -->\n', X)
