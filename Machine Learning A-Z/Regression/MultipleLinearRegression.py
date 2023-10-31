33c7bb626510babf86beb181f3c6c93382abcc58
Sami Munir <smunir2001@gmail.com>
2023-10-31 03:02:12 -0400

    encoding categorical data

--- Machine Learning A-Z/Regression/MultipleLinearRegression.py
+++ Machine Learning A-Z/Regression/MultipleLinearRegression.py
@@ -15,8 +15,15 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('./data/Data.csv')
+dataset = pd.read_csv('./data/Startups_Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 print('\nX -->\n', X)
 print('\ny -->\n', y)
+
+# Encoding categorical data
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+print('\nX -->\n', X)
