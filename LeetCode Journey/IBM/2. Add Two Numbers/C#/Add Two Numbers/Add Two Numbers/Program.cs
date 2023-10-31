f1691524f81a05bcae7a93f0a2e33210c837be27
Sami Munir <smunir2001@gmail.com>
2023-10-31 05:57:00 -0400

    adding LeetCode #2. Add Two Numbers

--- LeetCode Journey/IBM/2. Add Two Numbers/C#/Add Two Numbers/Add Two Numbers/Program.cs
+++ LeetCode Journey/IBM/2. Add Two Numbers/C#/Add Two Numbers/Add Two Numbers/Program.cs
@@ -1,7 +1,35 @@
-﻿public class Program {
+﻿using Add_Two_Numbers;
+
+public class Program {
     public static void Main(string[] args) {
         Console.WriteLine("2. Add Two Numbers");
 
-        //
+        ListNode l1a = new ListNode(2);
+        ListNode l1b = new ListNode(4);
+        ListNode l1c = new ListNode(3);
+        l1a.next = l1b;
+        l1b.next = l1c;
+        printList(l1a);
+
+        ListNode l2a = new ListNode(5);
+        ListNode l2b = new ListNode(6);
+        ListNode l2c = new ListNode(4);
+        l2a.next = l2b;
+        l2b.next = l2c;
+        printList(l2a);
+
+        Solution solObj = new Solution();
+        ListNode output = solObj.AddTwoNumbers(l1a, l2a);
+        printList(output);
+    }
+
+    private static void printList(ListNode head) {
+        ListNode currentNode = head;
+        Console.Write("list --> ");
+        while (currentNode != null) {
+            Console.Write(currentNode.val + " ");
+            currentNode = currentNode.next;
+        }
+        Console.WriteLine();
     }
 }
--- LeetCode Journey/IBM/2. Add Two Numbers/C#/Add Two Numbers/Add Two Numbers/Solution.cs
+++ LeetCode Journey/IBM/2. Add Two Numbers/C#/Add Two Numbers/Add Two Numbers/Solution.cs
@@ -5,10 +5,43 @@ public class Solution {
 
     /*
         Solution
-        Time complexity: O()
-        Space complexity: O()
-        Runtime: ms
-        Memory: MB
+        Time complexity: O(n)
+        Space complexity: O(1)
+        Runtime: 84ms
+        Memory: 49.63MB
     */
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode l3 = new ListNode(0);
+        ListNode tempHead = l3;
+        int carry = 0;
+        while (l1 != null && l2 != null) {
+            int currentValue = l1.val + l2.val + carry;
+            carry = currentValue / 10;
+            l3.next = new ListNode((currentValue % 10));
+            l3 = l3.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        while (l1 != null) {
+            int currentValue = l1.val + carry;
+            carry = currentValue / 10;
+            l3.next = new ListNode((currentValue % 10));
+            l3 = l3.next;
+            l1 = l1.next;
+        }
 
+        while (l2 != null) {
+            int currentValue = l2.val + carry;
+            carry = currentValue / 10;
+            l3.next = new ListNode((currentValue % 10));
+            l3 = l3.next;
+            l2 = l2.next;
+        }
+
+        if (carry != 0) {
+            l3.next = new ListNode(carry);
+        }
+
+        return tempHead.next;
+    }
 }
