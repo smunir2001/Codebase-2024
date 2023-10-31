4770d6d54a2fe5bd96e827edad6f6cfc568a3d27
Sami Munir <smunir2001@gmail.com>
2023-10-31 04:04:59 -0400

    adding ListNode obj & Solution.cs

---
+++ LeetCode Journey/IBM/2. Add Two Numbers/C#/Add Two Numbers/Add Two Numbers/Solution.cs
@@ -0,0 +1,14 @@
+﻿namespace Add_Two_Numbers;
+
+public class Solution {
+    public Solution() {}
+
+    /*
+        Solution
+        Time complexity: O()
+        Space complexity: O()
+        Runtime: ms
+        Memory: MB
+    */
+
+}
