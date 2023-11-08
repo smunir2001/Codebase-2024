ec08143e0d37955e6c344266f6c181ca9dae12bb
Sami Munir <smunir2001@gmail.com>
2023-11-07 22:20:36 -0500

    adding proj#2: AI Face Detection

---
+++ My CS Projects/AI Face Detection/Face Detector/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+    Sami Munir
+    smunir2001@gmail.com
+    Rutgers University
+    November 7th, 2023
+    AI Face Detection - C#
+*/
+class Program {
+    public static void Main(string[] args) {
+        Console.WriteLine("AI Face Detection");
+        Console.WriteLine("-----------------\n");
+    }
+}
--- My CS Projects/ReadMe.md
+++ My CS Projects/ReadMe.md
@@ -2,3 +2,4 @@ Sami Munir | Rutgers University | smunir2001@gmail.com
 # My CS Projects
 ## Directory
 ### AI Airspace - *Python, pygame*
+### AI Face Detection - *C#, OpenCV*
