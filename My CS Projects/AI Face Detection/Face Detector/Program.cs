eddc90940d70c60353aa0471be73e91a0f4fe79d
Sami Munir <smunir2001@gmail.com>
2023-11-07 22:36:27 -0500

    work in progress...

--- My CS Projects/AI Face Detection/Face Detector/Program.cs
+++ My CS Projects/AI Face Detection/Face Detector/Program.cs
@@ -5,6 +5,14 @@
     November 7th, 2023
     AI Face Detection - C#
 */
+
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Dnn;
+using System;
+using System.Drawing;
+
 class Program {
     public static void Main(string[] args) {
         Console.WriteLine("AI Face Detection");
