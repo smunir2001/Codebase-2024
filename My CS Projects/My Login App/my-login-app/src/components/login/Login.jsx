69bd879587f62b0ac130480a1f15ce133d6ee4bd
Sami Munir <smunir2001@gmail.com>
2023-11-09 14:51:04 -0500

    user authentication with error mssg display

--- My CS Projects/My Login App/my-login-app/src/components/login/Login.jsx
+++ My CS Projects/My Login App/my-login-app/src/components/login/Login.jsx
@@ -7,7 +7,12 @@ class Login extends Component {
         this.state = {
             username: '',
             password: '',
+            error: null,
         };
+
+    // Stored username & password
+    this.validUsername = 'sambreezy';
+    this.validPassword = 'sami123';
     }
 
     handleInputChange = (event) => {
@@ -17,16 +22,27 @@ class Login extends Component {
 
     handleFormSubmit = (event) => {
         event.preventDefault();
-        // Add logic here...
-        console.log('Username: ', this.state.username);
-        console.log('Password: ', this.state.password);
-        alert('login successful');
+        const {username, password} = this.state;
+        if (username === this.validUsername && password === this.validPassword) {
+            /*
+                Authentication succesful
+                - set a state variable or take some action
+                - redirect user or show successful login mssg or screen
+            */
+           alert('Login successful!')
+           this.setState({error: null}); // Clear any previous errors
+        } else {
+            // Authentication failed
+            // alert('Login failed. Please try again.')
+            this.setState({error: 'Invalid username or password.'});
+        }
     };
 
     render() {
         return (
             <div className='login-container'>
                 <h2 className='login-title'>Login</h2>
+                {this.state.error && <div className='error'>{this.state.error}</div>}
                 <form onSubmit={this.handleFormSubmit}>
                     <div className='form-container' id='form-username-container'>
                         <label>Username:</label>
