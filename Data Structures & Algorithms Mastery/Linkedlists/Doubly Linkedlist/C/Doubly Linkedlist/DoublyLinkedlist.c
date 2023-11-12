568428b99a4cb4f6db31e2e836b1696bde965da6
Sami Munir <smunir2001@gmail.com>
2023-11-12 02:47:03 -0500

    adding util functions doubly linkedlist - C

--- Data Structures & Algorithms Mastery/Linkedlists/Doubly Linkedlist/C/Doubly Linkedlist/DoublyLinkedlist.c
+++ Data Structures & Algorithms Mastery/Linkedlists/Doubly Linkedlist/C/Doubly Linkedlist/DoublyLinkedlist.c
@@ -10,8 +10,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Node {
+    int data;
+    struct Node *next;
+    struct Node *prev;
+} *head, *tail;
+
+int totalElements = 0;
+int headPointer = -1;
+int tailPointer = -1;
+
+int isEmpty() {
+    if (head == NULL || tail == NULL) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void printDS() {
+    if (isEmpty() == 1) {
+        printf("\tHEAD <-> TAIL\n");
+    } else {
+        printf("\tHEAD <-> ");
+        struct Node *currentNode = (struct Node*) malloc(sizeof(struct Node*));
+        while (currentNode != NULL) {
+            printf("%d <-> ", currentNode -> data);
+            currentNode = currentNode -> next;
+        }
+        printf("TAIL\n");
+    }
+}
+
+void printStats() {
+    printf("\nDS Stats...\n");
+    printf("total elements: %d\n", totalElements);
+    printf("headPointer: %d\n", headPointer);
+    printf("tailPointer: %d\n", tailPointer);
+    printDS();
+}
+
 int main(int argc, char* argv[]) {
     printf("Doubly Linkedlists in C\n");
     printf("-----------------------\n");
+
+    printStats();
     return EXIT_SUCCESS;
 }
