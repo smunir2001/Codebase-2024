fcd5fa48d95322a237608abb2fec91e6737d8fc5
Sami Munir <smunir2001@gmail.com>
2023-11-11 20:57:14 -0500

    flight data visualization proj complete (enhancements possible)

--- My CS Projects/Flight Data Visualization/Main.py
+++ My CS Projects/Flight Data Visualization/Main.py
@@ -24,3 +24,25 @@ def create_airline_bar_chart(data):
     if not data:
         return
     airlines = {}
+    for flight in data:
+        airline = flight['airline']['name']
+        airlines[airline] = airlines.get(airline, 0) + 1
+    sorted_airlines = sorted(airlines.items(), key=lambda x: x[1], reverse=True)
+    names, counts = zip(*sorted_airlines)
+    print('\ncounts -->\n', counts)
+    plt.figure(figsize=(10, 6))
+    plt.bar(names, counts)
+    plt.xlabel('Airlines')
+    plt.ylabel('Number of Flights')
+    plt.title('Number of Flights by Airline')
+    plt.xticks(rotation=45, ha='right', rotation_mode='anchor')
+    plt.subplots_adjust(bottom=0.4)
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == '__main__':
+    API_KEY = 'a008b8e7d69952bfbc6cd33ff6ffc2c8'
+    flight_data = fetch_live_flight_data(API_KEY)
+    print('\n*** Verified ***')
+    if flight_data:
+        create_airline_bar_chart(flight_data)
