11d85d17c838b385af9feb2c58948bb87a86201e
Sami Munir <smunir2001@gmail.com>
2023-11-12 11:55:46 -0500

    doubly linkedlist algo updates + function removeLast() + testing

--- Data Structures & Algorithms Mastery/Linkedlists/Doubly Linkedlist/C/Doubly Linkedlist/Doubly_Linkedlist.c
+++ Data Structures & Algorithms Mastery/Linkedlists/Doubly Linkedlist/C/Doubly Linkedlist/Doubly_Linkedlist.c
@@ -45,6 +45,10 @@ void printDS() {
 
 void printStats() {
     printf("\nDS Stats -->\n");
+    if (!isEmpty()) {
+        printf("HEAD -> data: %d\n", head -> data);
+        printf("TAIL -> data: %d\n", tail -> data);
+    }
     printf("total elements: %d\n", totalElements);
     printf("headPointer: %d\n", headPointer);
     printf("tailPointer: %d\n", tailPointer);
@@ -67,6 +71,9 @@ void insertFirst(int data) {
         struct Node *newNode = (struct Node*) malloc(sizeof(struct Node));
         newNode -> data = data;
         tempHead -> prev = newNode;
+        if (tempHead -> next == NULL) {
+            tail = tempHead;
+        }
         newNode -> next = tempHead;
         newNode -> prev = NULL;
         head = newNode;
@@ -108,14 +115,12 @@ void insertLast(int data) {
         headPointer = tailPointer = 0;
     } else {
         if (head -> next == NULL) {
-            struct Node *tempHead = head;
-            struct Node *currentHead = tempHead;
             struct Node *newNode = (struct Node*) malloc(sizeof(struct Node));
             newNode -> data = data;
             newNode -> next = NULL;
-            newNode -> prev = tail;
-            tempHead -> next = newNode;
-            head = tempHead;
+            newNode -> prev = head;
+            tail = newNode;
+            head -> next = newNode;
             totalElements++;
             tailPointer++;
         } else {
@@ -132,8 +137,40 @@ void insertLast(int data) {
             newNode -> prev = currentNode;
             newNode -> next = NULL;
             currentNode -> next = newNode;
+            tail = currentNode -> next;
             totalElements++;
             tailPointer++;
+        }
+    }
+    printStats();
+}
+
+void removeLast() {
+    if (isEmpty()) {
+        printf("\nremoveLast() called...");
+        printf("\n\t--<ERROR>-- linkedlist is empty.");
+    } else {
+        printf("\nremoveLast(%d) called...", tail -> data);
+        if (head -> next == NULL) {
+            head = tail = NULL;
+            totalElements = 0;
+            headPointer = tailPointer = -1;
+            free(head);
+            free(tail);
+        } else {
+            struct Node *tempHead = head;
+            struct Node *currentNode = tempHead;
+            while (currentNode != NULL) {
+                if (currentNode -> next -> next == NULL) {
+                    break;
+                }
+                currentNode = currentNode -> next;
+            }
+            tail = tail -> prev;
+            currentNode -> next = NULL;
+            head = tempHead;
+            totalElements--;
+            tailPointer--;
         }
     }
     printStats();
@@ -149,10 +186,8 @@ int main(int argc, char* argv[]) {
     insertFirst(2);
     insertFirst(-11);
     insertFirst(65);
-    insertFirst(7);
 
     // testing function removeFirst()
-    removeFirst();
     removeFirst();
     removeFirst();
     removeFirst();
@@ -162,7 +197,12 @@ int main(int argc, char* argv[]) {
     insertLast(2);
     insertLast(-11);
     insertLast(65);
-    insertLast(7);
+
+    // testing function removeLast()
+    removeLast();
+    removeLast();
+    removeLast();
+    removeLast(); // ERROR expected --> NULL linkedlist removal
 
     return EXIT_SUCCESS;
 }
