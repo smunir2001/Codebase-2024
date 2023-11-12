e77e5b3b5e4eb06c572841353e4a85fa60addf7c
Sami Munir <smunir2001@gmail.com>
2023-11-11 23:54:27 -0500

    adding Doubly Linkedlists - C#

---
+++ Data Structures & Algorithms Mastery/Linkedlists/Doubly Linkedlist/C#/Doubly Linkedlists/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+    Sami Munir
+    smunir2001@gmail.com
+    November 11, 2023
+    Rutgers University
+    Data Structures & Algorithms Mastery
+    - Doubly linkedlists
+*/
+
+public class Program {
+    public static void Main(string[] args) {
+        Console.WriteLine("Doubly Linkedlists - C#");
+        Console.WriteLine("-----------------------\n");
+    }
+}
