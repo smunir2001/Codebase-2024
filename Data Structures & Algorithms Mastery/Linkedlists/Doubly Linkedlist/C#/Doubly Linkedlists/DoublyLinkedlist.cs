26c29db9e1161de4cbfeab33adefc136a7f0017d
Sami Munir <smunir2001@gmail.com>
2023-11-12 01:29:03 -0500

    adding util functions to DoublyLL obj

--- Data Structures & Algorithms Mastery/Linkedlists/Doubly Linkedlist/C#/Doubly Linkedlists/DoublyLinkedlist.cs
+++ Data Structures & Algorithms Mastery/Linkedlists/Doubly Linkedlist/C#/Doubly Linkedlists/DoublyLinkedlist.cs
@@ -9,6 +9,42 @@ public class DoublyLinkedlist {
 
     public DoublyLinkedlist() {}
 
+    private Boolean isEmpty() {
+        if (head == null || tail == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void printDS() {
+        if (isEmpty()) {
+            Console.Write("\tHEAD <-> NULL");
+        } else {
+            Console.Write("\tHEAD <-> ");
+            Node currentNode = this.head;
+            while (currentNode != null) {
+                Console.Write(currentNode.getData() + " <-> ");
+                currentNode = currentNode.getNext();
+            }
+            Console.Write("NULL");
+        }
+    }
+
+    private void printStats() {
+        if (isEmpty()) {
+            Console.WriteLine("HEAD.getData(): N/A");
+            Console.WriteLine("TAIL.getData(): N/A");
+        } else {
+            Console.WriteLine("HEAD.getData(): " + head.getData());
+            Console.WriteLine("TAIL.getData(): " + tail.getData());
+        }
+        Console.WriteLine("total elements: " + totalElements);
+        Console.WriteLine("HEAD pointer: " + headPointer);
+        Console.WriteLine("TAIL pointer: " + tailPointer);
+        printDS();
+    }
+
     public void insertFirst(int data) {}
 
     public void removeFirst() {}
