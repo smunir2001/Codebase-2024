79a937410e2d4081aea6d54aca52219166f1f55e
Sami Munir <smunir2001@gmail.com>
2023-11-11 20:25:00 -0500

    adding function fetch_live_flight_data()

--- My CS Projects/Flight Data Visualization/Main.py
+++ My CS Projects/Flight Data Visualization/Main.py
@@ -5,3 +5,17 @@
 
 print('Flight Data Visualization')
 print('-------------------------')
+
+import requests
+import matplotlib.pyplot as plt
+
+def fetch_live_flight_data(API_KEY):
+    base_URL = 'http://api.aviationstack.com/v1/flights'
+    try:
+        response = requests.get(f'{base_URL}?access_key={API_KEY}')
+        response.raise_for_status()
+        data = response.json()
+        return data['data']
+    except requests.exceptions.RequestException as e:
+        print(f'Error fethcing data: {e}')
+        return None
