476a57a93ad14b373eb55597d6d3b672ff4fd589
Sami Munir <smunir2001@gmail.com>
2023-11-12 02:22:37 -0500

    adding Doubly Linkedlist DS - C

---
+++ Data Structures & Algorithms Mastery/Linkedlists/Doubly Linkedlist/C/Doubly Linkedlist/DoublyLinkedlist.c
@@ -0,0 +1,17 @@
+/*
+    Sami Munir
+    smunir2001@gmail.com
+    November 12, 2023
+    Rutgers University
+    Data Structures & Algorithms Mastery
+    - Doubly Linkedlists: C
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    printf("Doubly Linkedlists in C\n");
+    printf("-----------------------\n");
+    return EXIT_SUCCESS;
+}
