3730d6561dcf4887d3c8d7224eae8d7dd8ddd8b4
Sami Munir <smunir2001@gmail.com>
2023-11-28 22:22:32 -0500

    Edge obj complete

--- Data Structures & Algorithms Mastery/Graphs/Simple Graph/Java/Simple Graph/src/Edge.java
+++ Data Structures & Algorithms Mastery/Graphs/Simple Graph/Java/Simple Graph/src/Edge.java
@@ -1,2 +1,43 @@
 public class Edge {
+    Vertex src;
+    Vertex dest;
+    int weight;
+
+    public Edge() {}
+
+    public Edge(Vertex src, Vertex dest) {
+        this.src = src;
+        this.dest = dest;
+        this.weight = 1;
+    }
+
+    public Edge(Vertex src, Vertex dest, int weight) {
+        this.src = src;
+        this.dest = dest;
+        this.weight = weight;
+    }
+
+    public void setSrc(Vertex src) {
+        this.src = src;
+    }
+
+    public Vertex getSrc() {
+        return this.src;
+    }
+
+    public void setDest(Vertex dest) {
+        this.dest = dest;
+    }
+
+    public Vertex getDest() {
+        return this.dest;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getWeight() {
+        return this.weight;
+    }
 }
