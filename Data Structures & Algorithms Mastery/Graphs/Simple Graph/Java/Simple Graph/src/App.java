e1618a9dd2829ecd98c8b944c6a63be3865c72d4
Sami Munir <smunir2001@gmail.com>
2023-11-28 22:13:36 -0500

    Vertex obj complete

--- Data Structures & Algorithms Mastery/Graphs/Simple Graph/Java/Simple Graph/src/App.java
+++ Data Structures & Algorithms Mastery/Graphs/Simple Graph/Java/Simple Graph/src/App.java
@@ -2,5 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Simple Graph - Java");
         System.out.println("-------------------\n");
+
+        Vertex v1 = new Vertex("Sami", 7);
     }
 }
--- Data Structures & Algorithms Mastery/Graphs/Simple Graph/Java/Simple Graph/src/Vertex.java
+++ Data Structures & Algorithms Mastery/Graphs/Simple Graph/Java/Simple Graph/src/Vertex.java
@@ -1,2 +1,31 @@
 public class Vertex {
+    private String data;
+    private int key;
+
+    public Vertex() {}
+
+    public Vertex(String data) {
+        this.data = data;
+    }
+
+    public Vertex(String data, int key) {
+        this.data = data;
+        this.key = key;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getData() {
+        return this.data;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public int getKey() {
+        return this.key;
+    }
 }
