afda7fb13b5f24184c18a7797609e950031f19e9
Sami Munir <smunir2001@gmail.com>
2023-11-30 00:51:19 -0500

    Navbar component update i really like it so far

--- My CS Projects/My Portfolio/my-portfolio/src/components/Navbar/Navbar.jsx
+++ My CS Projects/My Portfolio/my-portfolio/src/components/Navbar/Navbar.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import './Navbar.css';
 import { FaGithub } from "react-icons/fa";
 import { CiLinkedin } from "react-icons/ci";
+import { FaHome } from "react-icons/fa";
+import { FaTasks } from "react-icons/fa";
+import { FaLaptopCode } from "react-icons/fa";
 
 const Navbar = () => {
   return (
@@ -12,9 +15,9 @@ const Navbar = () => {
         </div>
         <div className='navbar-links-con'>
             <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/">Skills</a></li>
-                <li><a href="/">Projects</a></li>
+                <li><a href="/"><div><FaHome/><span className='navbar-links-con-link_text'>Home</span></div></a></li>
+                <li><a href="/"><div><FaTasks/><span className='navbar-links-con-link_text'>Skills</span></div></a></li>
+                <li><a href="/"><div><FaLaptopCode/><span className='navbar-links-con-link_text'>Projects</span></div></a></li>
             </ul>
         </div>
         <div className='navbar-socials-links-con'>
