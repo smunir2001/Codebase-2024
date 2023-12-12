a81a6d0c4d84717d2950efba5b9b4528efc0a19e
Sami Munir <smunir2001@gmail.com>
2023-12-12 12:39:41 -0500

    adding Linkedlist Node object

--- Data Structures & Algorithms Mastery/Queues/Linkedlist Queue/C#/Linkedlist Queue/Node.cs
+++ Data Structures & Algorithms Mastery/Queues/Linkedlist Queue/C#/Linkedlist Queue/Node.cs
@@ -4,4 +4,28 @@ public class Node {
     private int data;
     private Node next;
     private Node prev;
+
+    public void SetData(int data) {
+        this.data = data;
+    }
+
+    public int GetData() {
+        return this.data;
+    }
+
+    public void SetNext(Node next) {
+        this.next = next;
+    }
+
+    public Node GetNext() {
+        return this.next;
+    }
+
+    public void SetPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    public Node GetPrev() {
+        return this.prev;
+    }
 }
