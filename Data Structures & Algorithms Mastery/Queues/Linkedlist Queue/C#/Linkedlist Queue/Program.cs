2aadb9d201a37318f2a68dc38520caab6e28777d
Sami Munir <smunir2001@gmail.com>
2023-12-12 12:58:02 -0500

    testing functions Enqueue() + Dequeue()

--- Data Structures & Algorithms Mastery/Queues/Linkedlist Queue/C#/Linkedlist Queue/Program.cs
+++ Data Structures & Algorithms Mastery/Queues/Linkedlist Queue/C#/Linkedlist Queue/Program.cs
@@ -16,5 +16,16 @@ public class Program {
 
         LLQueue ll_queue = new LLQueue();
         ll_queue.ToString();
+
+        // testing function Enqueue()
+        ll_queue.Enqueue(2);
+        ll_queue.Enqueue(-11);
+        ll_queue.Enqueue(65);
+
+        // testing function Dequeue()
+        ll_queue.Dequeue();
+        ll_queue.Dequeue();
+        ll_queue.Dequeue();
+        ll_queue.Dequeue(); // ERROR expected --> null linkedlist removal
     }
 }
