e01b40b23e9f8f5403d8169a2244f46595ae5a57
Sami Munir <smunir2001@gmail.com>
2023-12-12 12:35:57 -0500

    adding data structure: linkedlist queue

---
+++ Data Structures & Algorithms Mastery/Queues/Linkedlist Queue/C#/Linkedlist Queue/Node.cs
@@ -0,0 +1,7 @@
+﻿namespace Linkedlist_Queue;
+
+public class Node {
+    private int data;
+    private Node next;
+    private Node prev;
+}
---
+++ Data Structures & Algorithms Mastery/Queues/Linkedlist Queue/C#/Linkedlist Queue/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+    Sami Munir
+    smunir2001@gmail.com
+    December 12th, 2023
+    Rutgers University
+    Data Structures & Algorithms Mastery
+    - Linkedlist Queues: C#
+*/
+
+namespace Linkedlist_Queue;
+
+public class Program {
+    public static void Main(string[] args) {
+        Console.WriteLine("Linkedlist Queues - C#");
+        Console.WriteLine("----------------------\n");
+    }
+}
