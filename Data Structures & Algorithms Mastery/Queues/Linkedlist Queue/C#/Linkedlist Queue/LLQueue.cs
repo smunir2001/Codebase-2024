1522d39c3eec8a73493049f05b360007aea93c3f
Sami Munir <smunir2001@gmail.com>
2023-12-12 12:45:06 -0500

    adding LLQueue.PrintDS() + LLQueue.PrintStats()

--- Data Structures & Algorithms Mastery/Queues/Linkedlist Queue/C#/Linkedlist Queue/LLQueue.cs
+++ Data Structures & Algorithms Mastery/Queues/Linkedlist Queue/C#/Linkedlist Queue/LLQueue.cs
@@ -14,4 +14,30 @@ public class LLQueue {
             return false;
         }
     }
+
+    private void PrintDS() {
+        if (IsEmpty()) {
+            Console.Write("\t\tHEAD <-> ");
+            Node currentNode = this.head;
+            while (currentNode != null) {
+                Console.Write(currentNode.GetData() + " <-> ");
+                currentNode = currentNode.GetNext();
+            }
+            Console.Write("NULL\n");
+        }
+    }
+
+    private void PrintStats() {
+        if (IsEmpty()) {
+            Console.WriteLine("\tHEAD.getData(): N/A");
+            Console.WriteLine("\tTAIL.getData(): N/A");
+        } else {
+            Console.WriteLine("\tHEAD.getData(): " + head.GetData());
+            Console.WriteLine("\tTAIL.getData(): " + tail.GetData());
+        }
+        Console.WriteLine("\ttotal elements: " + totalElements);
+        Console.WriteLine("\tHEAD pointer: " + headPointer);
+        Console.WriteLine("\tTAIL pointer: " + tailPointer);
+        PrintDS();
+    }
 }
