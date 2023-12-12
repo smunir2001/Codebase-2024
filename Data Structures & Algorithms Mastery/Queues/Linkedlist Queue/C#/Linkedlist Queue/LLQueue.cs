f501f4087643f6b38165760b09e411da78f87926
Sami Munir <smunir2001@gmail.com>
2023-12-12 12:42:31 -0500

    adding Linkedlist Queue object

---
+++ Data Structures & Algorithms Mastery/Queues/Linkedlist Queue/C#/Linkedlist Queue/LLQueue.cs
@@ -0,0 +1,9 @@
+﻿namespace Linkedlist_Queue;
+
+public class LLQueue {
+    private Node head;
+    private Node tail;
+    private int totalElements = 0;
+    private int headPointer = -1;
+    private int tailPointer = -1;
+}
